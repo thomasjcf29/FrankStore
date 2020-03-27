aa6ecaa176e88e368224d8de1b99269596195516
Tom Franklin <github@thomasfranklin.co.uk>
2020-03-27 19:21:27 +0000

    Some basic pixel lookup test.

--- Program/main.cpp
+++ Program/main.cpp
@@ -8,6 +8,14 @@ int main() {
     cout << "Width: " << test.getWidth() << endl;
     cout << "Height: " << test.getHeight() << endl;
     cout << "Pixel Colour: " << test.getHexColour(0, 0) << endl;
+    cout << "Max Pixels: " << test.getMaxPixels() << endl;
     test.close();
+
+    for(int x = 0; x < test.getWidth(); x++){
+        for(int y = 0; y < test.getHeight(); y++){
+            cout << "Pixel Colour, X: " << x << ", Y: " << y << ", Colour: " << test.getHexColour(x, y) << endl;
+        }
+    }
+
     return 0;
 }
