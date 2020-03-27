6db8ea6ce76d7f3d77b472b46d1c7a4b609c027a
Tom Franklin <github@thomasfranklin.co.uk>
2020-03-27 23:32:15 +0000

    Added SHA512 Hash Support (For Pixel Information)

--- Program/main.cpp
+++ Program/main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
 #include "CoverImage.h"
+#include <iostream>
+#include <string.h>
+#include "sha512.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char **argv) {
     CoverImage test = CoverImage("/home/thomasjcf21/download.jpg");
     cout << "Width: " << test.getWidth() << endl;
     cout << "Height: " << test.getHeight() << endl;
@@ -13,9 +15,15 @@ int main() {
 
     for(int x = 0; x < test.getWidth(); x++){
         for(int y = 0; y < test.getHeight(); y++){
-            cout << "Pixel Colour, X: " << x << ", Y: " << y << ", Colour: " << test.getHexColour(x, y) << endl;
+            string colour = test.getHexColour(x, y);
+            string sx,sy;
+            sx = to_string(x);
+            sy = to_string(y);
+            string hash = sha512(sx + sy + colour);
+            cout << "Pixel Colour, X: " << x << ", Y: " << y << ", Colour: "
+                 << colour << ", Hash: " << hash << endl;
         }
     }
 
-    return 0;
+  	return 1;
 }
