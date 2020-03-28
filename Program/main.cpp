0a63ae4299b1327b1722bc870cdbb6f19795049e
Tom Franklin <github@thomasfranklin.co.uk>
2020-03-28 18:16:45 +0000

    Main class should have no actions, except for outsourcing.

--- Program/main.cpp
+++ Program/main.cpp
@@ -1,12 +1,6 @@
-#include "CoverImage.h"
-#include "CoverPixel.h"
-#include "Converter.h"
 #include <iostream>
 #include <string.h>
-#include <stdlib.h>
-#include <vector>
-#include <algorithm>
-
+#include "FrankEncode.h"
 
 using namespace std;
 
@@ -31,17 +25,6 @@ bool check(int argc, char **argv) {
     }
 }
 
-struct compare
-{
-	string key;
-	compare(string const &i): key(i) { }
-
-	bool operator()(string const &i)
-	{
-		return (i == key);
-	}
-};
-
 int main(int argc, char **argv) {
 
 	if(!check(argc, argv)){
@@ -49,37 +32,9 @@ int main(int argc, char **argv) {
 		exit(2);
 	}
 
-    CoverImage test = CoverImage("/home/thomasjcf21/download.jpg");
-    cout << "Width: " << test.getWidth() << endl;
-    cout << "Height: " << test.getHeight() << endl;
-    cout << "Max Pixels: " << test.getMaxPixels() << endl;
-
-    vector<string> pixelsUsed;
-    CoverPixel pixels[1000];
-
-    for(int i = 0; i < 1000; i++){
-        bool invalid = true;
-        do{
-            int * loc = test.getRandomLocation();
-            string x,y,key;
-            x = to_string(loc[0]);
-            y = to_string(loc[1]);
-            key = x + "-" + y;
-
-            if(any_of(pixelsUsed.begin(), pixelsUsed.end(), compare(key))){
-                invalid = true;
-                cout << "Element Found" << endl;
-            }
-            else{
-                invalid = false;
-                pixelsUsed.push_back(key);
-                string colour = test.getHexColour(loc[0], loc[1]);
-                pixels[i] = CoverPixel(loc[0], loc[1], colour);
-            }
-
-        } while(invalid);
+    if(strcmp(argv[1], "encode") == 0){
+        FrankEncode encoder = FrankEncode(argv);
     }
 
-    test.close();
   	return 0;
 }
