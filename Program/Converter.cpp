dfce64941f1dee48f41881461e6e176c75f67250
Tom Franklin <github@thomasfranklin.co.uk>
2020-03-29 05:43:33 +0100

    Convert hex colour to rgb colour.

--- Program/Converter.cpp
+++ Program/Converter.cpp
@@ -15,6 +15,32 @@ string Converter::rgb2hex(int r, int g, int b, bool with_head){
   return ss.str();
 }
 
+int* Converter::hex2rgb(string hex){
+    int* rgb = new int[3];
+    stringstream ss;
+    string str;
+
+    // Drop a hash if the value has one
+    if (hex[0] == '#') {
+    	hex.erase(0,1);
+    }
+
+    int size = hex.size();
+
+    for (int i = 0; i < 3; i++) {
+    	// Determine 3 or 6 character format.
+    	if (size == 3) { str = string(2, hex[i]); }
+    	else if (size == 6) { str = hex.substr(i * 2, 2); }
+    	else { break; }
+
+    	ss << std::hex << str;
+    	ss >> rgb[i];
+    	ss.clear();
+    }
+
+    return rgb;
+}
+
 int Converter::hex2int(string hex){
     return stoul(hex, nullptr, 16);
 }
