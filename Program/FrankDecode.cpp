c094bbc63d4464aae61b724c46121c313507fe02
Tom Franklin <github@thomasfranklin.co.uk>
2020-04-05 16:34:56 +0100

    Decode manager has been created to handle decoding.

---
+++ Program/FrankDecode.cpp
@@ -0,0 +1,66 @@
+#include "FrankDecode.h"
+#include "CoverImage.h"
+#include "Converter.h"
+#include "CoverPixel.h"
+#include "ImageToOutput.h"
+#include "FileToOutput.h"
+#include "ImageToRead.h"
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+FrankDecode::FrankDecode(char **argv, bool isImage){
+    cout << "[INFO]: Opening cover image." << endl;
+    image = CoverImage(argv[2]);
+
+	isImageInput = isImage;
+
+    if(isImageInput){
+        cout << "[INFO]: Opening image to decode." << endl;
+        inputImage = ImageToRead(argv[3]);
+
+        if(!inputImage.isValid()){
+            cout << "Could not read input image, exiting!" << endl;
+            exit(3);
+        }
+    }
+    else{
+        cout << "[INFO]: Opening file to decode." << endl;
+    }
+
+    if(!image.isValid()){
+        cout << "Could not read cover file, closing program." << endl;
+        exit(3);
+    }
+
+    cout << "[INFO]: Creating output file." << endl;
+    outputFile = FileToOutput(argv[4]);
+
+    if(!outputFile.isValid()){
+        cout << "[ERROR]: Program exiting" << endl;
+        exit(7);
+    }
+}
+
+void FrankDecode::decodeImage(){
+    bool moreToRead = true;
+    do{
+        Location* locations = inputImage.getNextPixels();
+        const char* strHex = image.getHexCode(locations, inputImage.getBufferSize());
+        outputFile.writetest(strHex);
+        delete [] strHex;
+        moreToRead = !inputImage.isRead();
+    } while(moreToRead);
+
+    outputFile.flush();
+}
+
+void FrankDecode::decode(){
+    cout << "[INFO]: Writing decode file." << endl;
+    if(isImageInput){
+        decodeImage();
+    }
+    cout << "[INFO]: Decode file has been written." << endl;
+}
---
+++ Program/FrankDecode.h
@@ -0,0 +1,34 @@
+#ifndef FRANKSTORE_FRANKDECODE_H
+#define FRANKSTORE_FRANKDECODE_H
+
+#include "CoverImage.h"
+#include "CoverPixel.h"
+#include "ImageToOutput.h"
+#include "FileToOutput.h"
+#include "ImageToRead.h"
+#include <string>
+
+#ifndef FRANKSTORE_LOCATION
+#define FRANKSTORE_LOCATION
+    struct Location{
+        int x, y, hash;
+    };
+#endif //FRANKSTORE_LOCATION
+
+class FrankDecode{
+private:
+    CoverImage image;
+    bool isImageInput;
+    int counter = 0;
+    FileToOutput outputFile;
+    ImageToRead inputImage;
+
+    void decodeImage();
+    void decodeFile();
+public:
+    FrankDecode(char **argv, bool isImage);
+    void decode();
+    void close();
+};
+
+#endif //FRANKSTORE_FRANKDECODE_H
