f29bd048117c3a8035e1e90e03de177f84c7aea0
Tom Franklin <github@thomasfranklin.co.uk>
2020-04-24 11:17:12 +0100

    Setup different options for initializer

--- Program/object_files/FrankEncrypt.cpp
+++ Program/object_files/FrankEncrypt.cpp
@@ -1,7 +1,30 @@
 #include "../header_files/FrankEncrypt.h"
 
 FrankEncrypt::FrankEncrypt(int argc, char **argv){
+    cout << "[INFO]: Setting up encryptor." << endl;
 
+    //Password - Not Specified
+    if(argc == 4){
+        action = Password;
+    }
+    //Image or Password Specified
+    else if(argc == 6){
+        action = Image;
+        coverImage = CoverImage(argv[5]);
+        valid = coverImage.isValid();
+    }
+    //Image and Password (Password Not Specified)
+    else if (argc == 7){
+        action = ImageAndPassword;
+        coverImage = CoverImage(argv[6]);
+        valid = coverImage.isValid();
+    }
+    //Image and Password (Password Specified)
+    else if(argc == 8){
+        action = ImageAndPassword;
+        coverImage = CoverImage(argv[7]);
+        valid = coverImage.isValid();
+    }
 }
 
 bool FrankEncrypt::isValid(){
