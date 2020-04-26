29aecd0c821013068237d7ee17f000e60591807e
Tom Franklin <github@thomasfranklin.co.uk>
2020-04-26 18:28:46 +0100

    Added test code to ensure image reading is correct.

--- Program/object_files/ExifManager.cpp
+++ Program/object_files/ExifManager.cpp
@@ -1,13 +1,38 @@
 #include "../header_files/ExifManager.h"
 #include <string.h>
 #include <exiv2/exiv2.hpp>
+#include <iomanip>
+#include <cassert>
 
 using namespace std;
+using namespace exiv2;
 
 ExifManager::ExifManager(){
 
 }
 
 ExifManager::ExifManager(string imageLocation){
-
+    Image::AutoPtr image = Exiv2::ImageFactory::open(imageLocation);
+    assert(image.get() != 0);
+    image->readMetadata();
+    Exiv2::ExifData &exifData = image->exifData();
+    if (exifData.empty()) {
+        std::string error("No Exif data found in file");
+        throw Exiv2::Error(Exiv2::kerErrorMessage, error);
+    }
+    Exiv2::ExifData::const_iterator end = exifData.end();
+    for (Exiv2::ExifData::const_iterator i = exifData.begin(); i != end; ++i) {
+        const char* tn = i->typeName();
+        std::cout << std::setw(44) << std::setfill(' ') << std::left
+                  << i->key() << " "
+                  << "0x" << std::setw(4) << std::setfill('0') << std::right
+                  << std::hex << i->tag() << " "
+                  << std::setw(9) << std::setfill(' ') << std::left
+                  << (tn ? tn : "Unknown") << " "
+                  << std::dec << std::setw(3)
+                  << std::setfill(' ') << std::right
+                  << i->count() << "  "
+                  << std::dec << i->value()
+                  << "\n";
+    }
 }
