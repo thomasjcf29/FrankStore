3e4c4b62030b1e8ca75924e312b2baaf4d84f869
Tom Franklin <github@thomasfranklin.co.uk>
2020-04-26 21:30:11 +0100

    Trying this.

--- Program/header_files/ExifManager.h
+++ Program/header_files/ExifManager.h
@@ -8,7 +8,7 @@ class ExifManager{
 private:
     bool valid = false;
     size_t length;
-    Exiv2::Image::AutoPtr *image;
+    Exiv2::Image::AutoPtr &image;
 public:
     ExifManager();
     ExifManager(char* imageLocation);
