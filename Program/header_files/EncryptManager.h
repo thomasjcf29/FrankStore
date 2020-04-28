58eda5bcda6e60d192ba8456415500ca42ad97ad
Tom Franklin <github@thomasfranklin.co.uk>
2020-04-28 18:14:31 +0100

    Added class initialisor

--- Program/header_files/EncryptManager.h
+++ Program/header_files/EncryptManager.h
@@ -4,12 +4,19 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <fstream>
 
 class EncryptManager{
 private:
     static constexpr const char* tempSalt = "wD,Fvhjb2{y=5N^w";
+    unsigned char* key;
+    ifstream in;
+    ofstream out;
+    bool valid;
 public:
     EncryptManager();
+    EncryptManager(unsigned char* password, std::string inputFile, std::string outputFile);
+    bool isValid();
     static unsigned char* PBKDF2_HMAC_SHA_256(const char* pass, int passlen, int32_t iterations, uint32_t outputBytes);
 };
 
