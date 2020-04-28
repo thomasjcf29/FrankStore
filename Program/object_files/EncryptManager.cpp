010a2008e0731486c0c3eb7a68ecfed521b5a1f7
Tom Franklin <github@thomasfranklin.co.uk>
2020-04-28 13:36:25 +0100

    Test basic EncryptManager

---
+++ Program/object_files/EncryptManager.cpp
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <getopt.h>
+#include <ctype.h>
+#include <openssl/crypto.h>
+
+void PBKDF2_HMAC_SHA_256(const char* pass, int passlen, const unsigned char* salt, int saltlen, int32_t iterations, uint32_t outputBytes, char* hexResult, uint8_t* binResult)
+{
+    unsigned int i;
+    unsigned char digest[outputBytes];
+    PKCS5_PBKDF2_HMAC(pass, passlen, salt, saltlen, iterations, EVP_sha256(), outputBytes, digest);
+    for (i = 0; i < sizeof(digest); i++)
+    {
+        sprintf(hexResult + (i * 2), "%02x", 255 & digest[i]);
+        binResult[i] = digest[i];
+    };
+}
