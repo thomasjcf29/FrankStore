c4b8133c3a5d4a9f1d8ac58705899e81bc160557
Tom Franklin <github@thomasfranklin.co.uk>
2020-04-28 14:59:09 +0100

    Shouldn't be void void.

--- Program/header_files/EncryptManager.h
+++ Program/header_files/EncryptManager.h
@@ -9,7 +9,7 @@ class EncryptManager{
 private:
 public:
     EncryptManager();
-    static void void PBKDF2_HMAC_SHA_256(const char* pass, int passlen, const unsigned char* salt, int saltlen, int32_t iterations, uint32_t outputBytes, char* hexResult, uint8_t* binResult);
+    static void PBKDF2_HMAC_SHA_256(const char* pass, int passlen, const unsigned char* salt, int saltlen, int32_t iterations, uint32_t outputBytes, char* hexResult, uint8_t* binResult);
 };
 
 #endif //FRANKSTORE_ENCRYPTMANAGER_H
