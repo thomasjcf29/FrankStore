54e349eedd7d1f9a11a0410846b4302263f180ed
Tom Franklin <github@thomasfranklin.co.uk>
2020-05-03 13:59:02 +0100

    Basic UI creation

--- UI/main.cpp
+++ UI/main.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char **argv){
     cout << "Hello World!" << endl;
+
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+    Gtk::Window window;
+    window.set_default_size(200, 200);
+
+    return app->run(window);
 }
