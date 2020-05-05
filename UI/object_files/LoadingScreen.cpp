ecd9dc0c8242a23a728682a8a99878978f72f458
Tom Franklin <github@thomasfranklin.co.uk>
2020-05-05 15:30:16 +0100

    Test adding css file

--- UI/object_files/LoadingScreen.cpp
+++ UI/object_files/LoadingScreen.cpp
@@ -22,9 +22,18 @@ LoadingScreen::LoadingScreen(){
         return;
     }
 
+    auto css_provider = Gtk::CssProvider::create();
+    css_provider->load_from_resource("designs/application.css");
+
+    if(not css_provider){
+        std::cerr << "Cannot load CSS file" << std::endl;
+        return;
+    }
+
     refBuilder->get_widget("loadingScreen", pWindow);
     if(pWindow){
         pWindow->show_all();
+        Gtk::StyleContext::add_provider_for_screen(Gdk::Screen::get_default(), css_provider, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
         valid = true;
     }
 }
