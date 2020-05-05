7ba0687ddbc6a9a9beb3ba1b8b9838fdd5f7db18
Tom Franklin <github@thomasfranklin.co.uk>
2020-05-05 18:48:23 +0100

    Add root level

--- UI/main.cpp
+++ UI/main.cpp
@@ -9,6 +9,19 @@ int main(int argc, char **argv){
 
     auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
+    auto css_provider = Gtk::CssProvider::create();
+
+    css_provider->signal_parsing_error().connect(
+      [](const Glib::RefPtr<const Gtk::CssSection>& section,
+         const Glib::Error& error) {
+        std::cerr << "CSS Error: " << error.what() << std::endl;
+        std::exit(1);
+      });
+
+    css_provider->load_from_path("designs/application.css");
+
+    Gtk::StyleContext::add_provider_for_screen(Gdk::Screen::get_default(), css_provider, GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
     LoadingScreen loadingScreen;
 
     if(loadingScreen.isValid()){
